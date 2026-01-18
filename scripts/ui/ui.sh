1f0904dca9c1d970fa27b7f324a5e9e0b515025d
Lane Maynor <lane.motivated@gmail.com>
2026-01-18 14:31:58 -0500

    Fix: UI

--- scripts/ui/ui.sh
+++ scripts/ui/ui.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# source "$(dirname "${BASH_SOURCE[0]}")/../colors/colors.sh"
-SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
-. "$SCRIPT_DIR/scripts/colors/colors.sh"
-printf "DEGUB: ${SCRIPT_DIR}\n"
+
+. "$SCRIPT_DIR/colors/colors.sh"
 
 ui_git_branch() {
     if git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
